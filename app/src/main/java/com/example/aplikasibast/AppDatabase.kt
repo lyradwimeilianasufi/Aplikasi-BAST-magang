424faf6e931dee3e1c63756671868aee073ab18b
lyradwimeilianasufi <lyradms@gmail.com>
2026-05-21 14:01:25 +0700

    desain ornament 1&2

--- app/src/main/java/com/example/aplikasibast/AppDatabase.kt
+++ app/src/main/java/com/example/aplikasibast/AppDatabase.kt
@@ -3,8 +3,8 @@ package com.example.aplikasibast
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-// Menaikkan versi ke 3 untuk mereset skema database secara menyeluruh
-@Database(entities = [KehadiranEntity::class, PengajuanIzinEntity::class], version = 3)
+// Menaikkan versi ke 4 untuk mereset skema database karena ada perubahan entitas
+@Database(entities = [KehadiranEntity::class, PengajuanIzinEntity::class], version = 4)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun kehadiranDao(): KehadiranDao
     abstract fun pengajuanIzinDao(): PengajuanIzinDao
